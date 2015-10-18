0025a95ff1b83fcf89cd0075a5cc06bd709a363a
omochisaurus <omochisaurus@gamil.com>
2015-10-18 09:59:24 +0000

    課題4

---
+++ app/controllers/rankings_controller.rb
@@ -0,0 +1,13 @@
+class RankingsController < ApplicationController
+    
+    def have
+        ids = Have.group(:item_id).order("count_item_id desc").limit(10).count(:item_id).keys
+        @rank_have = Item.find(ids).sort_by{ |item| ids.index(item.id) }
+    end
+    
+    def want
+        ids = Want.group(:item_id).order("count_item_id desc").limit(10).count(:item_id).keys
+        @rank_want = Item.find(ids).sort_by{ |item| ids.index(item.id) }
+    end
+    
+end
