2fa66ee2c69576fb439af7dd10a4a5d6de1e22dc
omochisaurus <omochisaurus@gamil.com>
2015-10-18 12:30:21 +0000

    herokuエラー修整3

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,12 +1,12 @@
 class RankingsController < ApplicationController
     
     def have
-        ids = Have.group('items.id').order("count_item_id desc").limit(10).count(:item_id).keys
+        ids = Have.group(:item_id).order("count_item_id desc").limit(10).count(:item_id).keys
         @rank_have = Item.find(ids).sort_by{ |item| ids.index(item.id) }
     end
     
     def want
-        ids = Want.group('items.id').order("count_item_id desc").limit(10).count(:item_id).keys
+        ids = Want.group(:item_id).order("count_item_id desc").limit(10).count(:item_id).keys
         @rank_want = Item.find(ids).sort_by{ |item| ids.index(item.id) }
     end
     
